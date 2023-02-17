5fafe3af0c5f00b05c7fb4fd31abecab601ccdee
Asim <asimpsy11@gmail.com>
2023-02-17 22:24:39 +0100

    Main screen fine tuning

--- app/src/main/java/com/example/flickrfinder/componenet/PhotoItem.kt
+++ app/src/main/java/com/example/flickrfinder/componenet/PhotoItem.kt
@@ -1,17 +1,18 @@
 package com.example.flickrfinder.componenet
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.Text
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import coil.compose.rememberAsyncImagePainter
 import com.example.flickrfinder.model.PhotoData
+import com.example.flickrfinder.ui.theme.WhiteShadow
 
 @Composable
 fun PhotoItem(
@@ -23,20 +24,23 @@ fun PhotoItem(
         .clip(RoundedCornerShape(5.dp))
         .clickable { onItemClicked(item) }
     ) {
+        val scroll = rememberScrollState(0)
         Image(
             painter = rememberAsyncImagePainter(item.url),
             contentDescription = null,
             modifier = Modifier
                 .width(150.dp)
-                .height(100.dp)
-                .align(Alignment.CenterHorizontally)
+                .height(100.dp),
+            contentScale = ContentScale.Crop
         )
         Text(
             text = item.title,
             modifier = Modifier
+                .background(WhiteShadow)
                 .width(150.dp)
                 .padding(5.dp)
-                .align(Alignment.CenterHorizontally)
+                .horizontalScroll(scroll),
+            textAlign = TextAlign.Center
         )
     }
 }
