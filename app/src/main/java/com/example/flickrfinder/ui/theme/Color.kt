6983a9ab0f781837c85dbd850ce65798f3a5474f
Asim <asimpsy11@gmail.com>
2023-02-20 15:51:20 +0100

    Splash screen icon and color done

--- app/src/main/java/com/example/flickrfinder/ui/theme/Color.kt
+++ app/src/main/java/com/example/flickrfinder/ui/theme/Color.kt
@@ -6,4 +6,13 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
 val WhiteShadow = Color(0xFFF1F1F1)
+
+val colorPrimary = Color(0xFFFF7B7B)
+val colorRedDark = Color(0xFFFF7B7B)
+val colorRedLite = Color(0xFFFEA0A0)
+val colorWhite = Color(0xFFFFFFFF)
+val colorRedGrayLight= Color(0xFFFEE8E8)
+val colorGray= Color(0xFFFFEEEE)
+val colorBlack = Color(0xFF000000)
