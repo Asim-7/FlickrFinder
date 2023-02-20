6a99b808aa07d2daa8ced5160b3e53e7d36b3e58
Asim <asimpsy11@gmail.com>
2023-02-20 23:19:41 +0100

    Search box improved

--- app/src/main/java/com/example/flickrfinder/componenet/SearchScreen.kt
+++ app/src/main/java/com/example/flickrfinder/componenet/SearchScreen.kt
@@ -2,22 +2,26 @@ package com.example.flickrfinder.componenet
 
 import android.os.Handler
 import android.os.Looper
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.LocalView
@@ -25,6 +29,8 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.flickrfinder.ui.theme.colorRedDark
+import com.example.flickrfinder.ui.theme.colorWhite
 import com.example.flickrfinder.viewmodel.PhotoViewModel
 
 @Composable
@@ -134,7 +140,6 @@ fun QuerySearch(
     onQueryChanged: (String) -> Unit
 ) {
 
-    var showClearButton by remember { mutableStateOf(false) }
     val focusRequester = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
     val maxChar = 15
@@ -143,45 +148,79 @@ fun QuerySearch(
         focusRequester.requestFocus()
     }
 
-    OutlinedTextField(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(start = 20.dp, end = 20.dp, top = 20.dp)
-            .onFocusChanged { focusState ->
-                showClearButton = (focusState.isFocused)
-            }
-            .focusRequester(focusRequester),
-        value = query,
-        onValueChange = {
-            val allowedString = it.isAllowed()
-            if (allowedString.length <= maxChar) onQueryChanged(allowedString)
-        },
-        label = { Text(text = label) },
-        textStyle = MaterialTheme.typography.subtitle1,
-        singleLine = true,
-        trailingIcon = {
-            if (showClearButton) {
-                IconButton(onClick = { onClearClick() }) {
-                    Icon(imageVector = Icons.Filled.Close, contentDescription = "Clear")
+    Card(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(start = 20.dp, end = 20.dp, top = 20.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(24.dp)
+    ) {
+        TextField(
+            modifier = modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = colorWhite,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            shape = RoundedCornerShape(24.dp),
+            placeholder = {
+                Text(
+                    text = "Search pictures",
+                    color = Color.Gray
+                )
+            },
+            value = query,
+            onValueChange = {
+                val allowedString = it.isAllowed()
+                if (allowedString.length <= maxChar) onQueryChanged(allowedString)
+            },
+            textStyle = MaterialTheme.typography.subtitle1,
+            singleLine = true,
+            trailingIcon = {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(shape = CircleShape)
+                        .background(colorRedDark)
+                ) {
+                    IconButton(onClick = {
+                        if (query.isEmpty()) {
+                            onEmptyClick()
+                        } else {
+                            focusRequester.freeFocus()
+                            keyboardController?.hide()
+                            Handler(Looper.getMainLooper()).postDelayed({       // handler added to hide keyboard correctly
+                                onDoneActionClick()
+                            }, 0)
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Search,
+                            contentDescription = "Search",
+                            tint = colorWhite
+                        )
+                    }
                 }
-            }
-        },
-        keyboardActions = KeyboardActions(onDone = {
-            if (query.isEmpty()) {
-                onEmptyClick()
-            } else {
-                focusRequester.freeFocus()
-                keyboardController?.hide()
-                Handler(Looper.getMainLooper()).postDelayed({       // handler added to hide keyboard correctly
-                    onDoneActionClick()
-                }, 0)
-            }
-        }),
-        keyboardOptions = KeyboardOptions(
-            imeAction = ImeAction.Done,
-            keyboardType = KeyboardType.Text
+            },
+            keyboardActions = KeyboardActions(onDone = {
+                if (query.isEmpty()) {
+                    onEmptyClick()
+                } else {
+                    focusRequester.freeFocus()
+                    keyboardController?.hide()
+                    Handler(Looper.getMainLooper()).postDelayed({       // handler added to hide keyboard correctly
+                        onDoneActionClick()
+                    }, 0)
+                }
+            }),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Text
+            )
         )
-    )
+    }
 
 }
 
